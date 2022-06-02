31c54441b16a28375bfb62efe600acf2cb203aa7
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-02 14:45:54 +0200

    main components changed..

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import "./App.scss";
+import './App.scss';
+import { PageWelcome } from './pages/PageWelcome';
+import { PageBooks } from './pages/PageBooks';
+import { PageUseToggle } from './pages/PageUseToggle';
+import { PageAbout } from './pages/PageAbout';
+import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
-  return <div className="App">hello</div>;
+	return (
+		<div className="App">
+			<h1>React Custom Hooks</h1>
+			<hr />
+			<NavLink to="/welcome">Welcome</NavLink> |{' '}
+			<NavLink to="/books">Books</NavLink> |{' '}
+			<NavLink to="/use-toggle">UseToggle</NavLink> |{' '}
+			<NavLink to="/about">About</NavLink>
+			<hr />
+			<Routes>
+				<Route path="/welcome" element={<PageWelcome />} />
+				<Route path="/books" element={<PageBooks />} />
+				<Route path="/use-toggle" element={<PageUseToggle />} />
+				<Route path="/about" element={<PageAbout />} />
+				<Route path="/" element={<Navigate to="/welcome" replace />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
